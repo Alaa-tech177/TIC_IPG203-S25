ccb0e0792b4c0c10b425f1b02b685354563b129e
Alaa-tech177 <alaahasannani@outlook.com>
2026-05-25 13:36:56 -0400

    Add IRepairService interface for repair operations

---
+++ Interfaces/IRepairService.cs
@@ -0,0 +1,12 @@
+// =====--====================--===============
+// student contributing name: [Lama Mohammad Bakro]
+// =================--=========================
+
+namespace CarRepairCenter.Interfaces
+{
+    public interface IRepairService
+    {
+        void StartRepair();
+        string GetRepairSummary();
+    }
+}
